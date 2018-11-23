60b8d2408d14922fb69c025b79036461c98b08b9
afw821 <afw821@gmail.com>
2018-11-23 17:47:40 -0500

    I added a hover effects on the navbar links

--- form/contactform.php
+++ form/contactform.php
@@ -1,8 +1,15 @@
 <?php
 
-if(isset($_POST['submit']))[
+if(isset($_POST['submit'])){
     $firstName= $_POST['name'];
     $lastName= $_POST['name'];
     $mailFrom= $_POST['mail'];
     $message= $_POST['message'];
-]
+
+    $mailTo = "afw821@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $txt, $headers);
+    header("Location: form.php?mailsend");
+}
